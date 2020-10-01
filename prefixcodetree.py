dbf0494a5176154a997f5a3f1c55aa0429cf33fe
namdongngac <37470354+namdongngac@users.noreply.github.com>
2020-10-01 21:38:20 +0700

    week 4 prefixcodetree

---
+++ prefixcodetree.py
@@ -0,0 +1,31 @@
+class PrefixCodeTree:
+  def __init__(self):
+    self.tree = [0]*1000
+
+  def insert(self, codeword, symbol):
+    #find index of codeword
+    index=0
+    for element in codeword:
+      if element == 1:
+        index = index * 2 + 2
+      elif element == 0:
+        index = index * 2 + 1
+    self.tree[index] = symbol
+
+  def decode(self, encodedData, dataLen):
+    result = ""
+    index = 0
+    data = ""
+    for byte in encodedData:
+      data += f'{byte:0>8b}'
+    for i in range(dataLen):
+      char = data[i]
+      if char == '1':
+        index = index * 2 + 2
+      elif char == '0':
+        index = index * 2 + 1
+      if self.tree[index] != 0:
+        result += " " + self.tree[index]
+        index = 0
+    return result
+
